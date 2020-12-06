c4c63b2b39eb7799598c5ee543eef8774c66d593
Dustin Wagner <60157752+wagnerd97@users.noreply.github.com>
2020-12-05 19:01:43 -0700

    Update sql builder so phone numbers are strings

--- buildTables.sql
+++ buildTables.sql
@@ -23,7 +23,7 @@ create table Customers (
 	city varchar(20),
 	province varchar(20),
 	zip varchar(7),
-	phone_number bigint);
+	phone_number varchar(30));
 
 create table Car_types (
 	CarType varchar(30) primary key,
@@ -38,7 +38,7 @@ create table Branches (
 	city varchar(30),
 	province varchar(30),
 	zip varchar(7),
-	phone_number bigint);
+	phone_number varchar(30));
 
 create table Cars (
 	VIN varchar(17) primary key,
@@ -58,7 +58,7 @@ create table Employees (
 	city varchar(30),
 	province varchar(30),
 	zip varchar(7),
-	phone_number bigint,
+	phone_number varchar(30),
 	Branch_ID int foreign key references Branches(Branch_ID)
 );
 
@@ -74,9 +74,9 @@ create table Rental_trans (
 	VIN varchar(17) foreign key references Cars(VIN)
 );
 
-insert into Customers values(1, 'John', 'Hancock', 123456789, 12345, 'newStreet', 15, 0, 'Edmonton', 'Alberta', 'A1B 2C3', 1234567890);
+insert into Customers values(1, 'John', 'Hancock', 123456789, 12345, 'newStreet', 15, 0, 'Edmonton', 'Alberta', 'A1B 2C3', '1234567890');
 insert into Car_types values('Sedan', 100, 100, 100);
-insert into Branches values(1, 'St. Albert Trail', 140, 'Edmonton', 'AB', 'T6A 3M9', 7805555555);
+insert into Branches values(1, 'St. Albert Trail', 140, 'Edmonton', 'AB', 'T6A 3M9', '7805555555');
 insert into Cars values('S9G7H1KJY7H508JN7', 'red', 'Pontiac Sunfire', 'Sedan', 1);
-insert into Employees values(1, 'Joe', 'O','Bloe', 'Cherry St.', '69', 'Saskatoon', 'Saskatchewan', 'K9R 4Y7', 5645558790, 1);
+insert into Employees values(1, 'Joe', 'O','Bloe', 'Cherry St.', '69', 'Saskatoon', 'Saskatchewan', 'K9R 4Y7', '5645558790', 1);
 insert into Rental_trans values(1234, '2020-11-29', '2020-12-01', 300, 1, 1, 1, 1, 'S9G7H1KJY7H508JN7');
